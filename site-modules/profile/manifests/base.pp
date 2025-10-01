ab4173c793aa1eaefc19204060b417a7cf0fef7e
michaelkosko <michael.kosko@thinkiq.com>
2025-10-01 09:47:00 -0700

    moving ntp class to module profile

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,9 +1,5 @@
 # The base profile should include component modules that will be on all nodes
-class profile::base (
-  Array[String] $ntp_servers
-) {
-  class { 'ntp': 
-    servers => $ntp_servers,
-  },
+class profile::base {
+  include profile::ntp
   include profile::patching
 }
