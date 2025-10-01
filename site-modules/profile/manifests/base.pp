6ce2dbb3a3c3ab000fc990cd39df00d661693a09
michaelkosko <michael.kosko@thinkiq.com>
2025-10-01 09:40:25 -0700

    update base.pp

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,10 +1,9 @@
 # The base profile should include component modules that will be on all nodes
 class profile::base (
-  include profile::patching
-
   Array[String] $ntp_servers
 ) {
   class { 'ntp': 
     servers => $ntp_servers,
-  }
+  },
+  include profile::patching
 }
