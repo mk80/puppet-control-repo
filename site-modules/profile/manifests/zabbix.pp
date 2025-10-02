4d4cf1f947c7376f40266d6dcd7ed9621d248695
michaelkosko <michael.kosko@thinkiq.com>
2025-10-02 14:03:51 -0700

    switching to zabbix-agent since agent2 was defined and only agent was installed

--- site-modules/profile/manifests/zabbix.pp
+++ site-modules/profile/manifests/zabbix.pp
@@ -1,10 +1,4 @@
 class { 'zabbix::agent':
-  agent_configfile_path => '/etc/zabbix/zabbix_agent2.conf',
-  include_dir           => '/etc/zabbix/zabbix_agent2.d',
-  include_dir_purge     => false,
-  zabbix_package_agent  => 'zabbix-agent2',
-  servicename           => 'zabbix-agent2',
-  manage_startup_script => false,
   server                => 'quietbox.madhash.io',
   manage_repo           => true,
   zabbix_package_state  => 'latest',
